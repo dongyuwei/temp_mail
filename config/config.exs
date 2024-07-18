e4425927b343ad72ffa5e8444caa8b7a72066baf
Dong Yuwei <newdongyuwei@gmail.com>
2024-07-18 19:17:31 +0800

    relocate db path

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :phoenix, :json_library, Jason
 
 config :temp_mail, TempMail.Repo,
-  database: "temp_mail.sqlite3"
+  database: Path.expand("../priv/repo/db/temp_mail.sqlite3", __DIR__)
 
 config :temp_mail,
   ecto_repos: [TempMail.Repo]
